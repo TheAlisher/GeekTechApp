b8f44d7b2122a460795c04576493a13156e54114
macbookpro <abenov2005.uzer@gmail.com>
2020-08-28 23:01:31 +0600

    added -> WWE(who will eat) logic

--- app/src/main/java/com/alis/geektech/presentation/fragments/home/inoffice/InOfficeFragment.java
+++ app/src/main/java/com/alis/geektech/presentation/fragments/home/inoffice/InOfficeFragment.java
@@ -9,10 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.alis.geektech.R;
 
 public class InOfficeFragment extends Fragment {
+
+    private Button buttonWhoWillEat;
 
     public InOfficeFragment() {
     }
@@ -26,5 +30,22 @@ public class InOfficeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        initializationViews(view);
+
+        buttonWhoWillEat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickWhoWillEat();
+            }
+        });
+    }
+
+    private void initializationViews(View view) {
+        buttonWhoWillEat = view.findViewById(R.id.button_inOffice_who_will_eat);
+    }
+
+    private void clickWhoWillEat() {
+        Toast.makeText(getContext(), "Отправить всем кто в офисе уведомление КТО БУДЕТ КУШАТЬ?", Toast.LENGTH_SHORT).show();
     }
 }
