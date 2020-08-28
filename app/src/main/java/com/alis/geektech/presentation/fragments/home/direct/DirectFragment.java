fa7d7986cf97af533e17a92eacd254c5fb7a503e
macbookpro <abenov2005.uzer@gmail.com>
2020-08-28 20:43:25 +0600

    add -> viewPager with 3 fragments | add fragments -> QRScannerFragment, EventsFragment, DirectFragment

---
+++ app/src/main/java/com/alis/geektech/presentation/fragments/home/direct/DirectFragment.java
@@ -0,0 +1,30 @@
+package com.alis.geektech.presentation.fragments.home.direct;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alis.geektech.R;
+
+public class DirectFragment extends Fragment {
+
+    public DirectFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_direct, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/alis/geektech/presentation/fragments/home/qrscanner/QRScannerFragment.java
@@ -0,0 +1,30 @@
+package com.alis.geektech.presentation.fragments.home.qrscanner;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alis.geektech.R;
+
+public class QRScannerFragment extends Fragment {
+
+    public QRScannerFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_qr_scanner, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
