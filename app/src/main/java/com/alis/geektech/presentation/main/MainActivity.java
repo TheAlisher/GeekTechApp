a1ef5299321f5af94a6cfc70f55f5adf9c49c05d
macbookpro <abenov2005.uzer@gmail.com>
2020-08-28 19:22:11 +0600

    add -> BottomNavigationView with NavController | fragments EventsFragment & ProfileFragment

---
+++ app/src/main/java/com/alis/geektech/presentation/main/MainActivity.java
@@ -0,0 +1,28 @@
+package com.alis.geektech.presentation.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.alis.geektech.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setNavControllerWithBottomNav();
+    }
+
+    private void setNavControllerWithBottomNav() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
