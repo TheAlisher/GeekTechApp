88b7f3844cb807e897a73adb626109a8cc94bfca
macbookpro <abenov2005.uzer@gmail.com>
2020-08-30 17:57:58 +0600

    added -> splash_screen.xml light_dark

--- app/src/main/java/com/alis/geektech/presentation/main/MainActivity.java
+++ app/src/main/java/com/alis/geektech/presentation/main/MainActivity.java
@@ -37,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         isDarkMode();
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         isIntroOrAuthentication();
 
